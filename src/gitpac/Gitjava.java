bc834da215956625c357b26295bb8465389c84fe
SharathBabu Munukuntla <sharathbabu.m01@ad.infosys.com>
2020-06-23 16:43:37 +0530

    C5

---
+++ src/gitpac/Gitjava.java
@@ -0,0 +1,15 @@
+package gitpac;
+
+public class Gitjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("jjjjjjjjjj");
+
+System.out.println("jjjjjjjjjj");
+System.out.println("jjjjjjjjjj");
+
+
+	}
+
+}
